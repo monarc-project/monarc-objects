efa3b4c6d1f9e909e363a52668753d2e02b2fb35
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-11-18 21:17:21 +0100

    added echo to fix the problem of waiting time of travis

--- validate_all.sh
+++ validate_all.sh
@@ -6,7 +6,9 @@ for f in *; do
     if [[ -d $f ]]; then
         for object in $f/*.json
         do
+	    echo -n "${f}: "
             jsonschema -i ${object} $f-schema.json
+	    echo ''
         done
     fi
 done
