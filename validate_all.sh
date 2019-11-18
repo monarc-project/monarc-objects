1f0b9e9a3ebf08f5501992943f1c25ae7ad82056
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-11-18 21:34:20 +0100

    display the name of the JSON file during the test.

--- validate_all.sh
+++ validate_all.sh
@@ -6,7 +6,7 @@ for f in *; do
     if [[ -d $f ]]; then
         for object in $f/*.json
         do
-            echo -n "${f}: "
+            echo -n "${object}: "
             jsonschema -i ${object} $f-schema.json
             echo ''
         done
